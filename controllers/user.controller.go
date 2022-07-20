b220fc1cd8e7152e86430ceb0688aab821f41352
Bagus Wicaksono <baguswicaksono388@gmail.com>
2022-07-20 17:38:51 +0700

    API Create Data User with validate request

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -3,18 +3,60 @@ package controllers
 import (
 	"go-fiber-gorm/database"
 	"go-fiber-gorm/model/entity"
+	"go-fiber-gorm/model/request"
 	"log"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
 func UserControllerGetAll(ctx *fiber.Ctx) error {
 	var users []entity.User
-	result := database.DB.Find(&users)
+	result := database.DB.Debug().Find(&users)
 
 	if result.Error != nil {
 		log.Println(result.Error)
 	}
 
-	return ctx.JSON(users)
+	return ctx.JSON(fiber.Map{
+		"message": "Success get all user",
+		"data":    users,
+	})
+}
+
+func UserControllerCreate(ctx *fiber.Ctx) error {
+	user := new(request.UserCreateRequest)
+
+	if err := ctx.BodyParser(&user); err != nil {
+		return err
+	}
+
+	validate := validator.New()
+	errValidate := validate.Struct(user)
+	if errValidate != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Validation error",
+			"data":    errValidate.Error(),
+		})
+	}
+
+	newUser := entity.User{
+		Name:    user.Name,
+		Email:   user.Email,
+		Address: user.Address,
+		Phone:   user.Phone,
+	}
+
+	errorCreateUser := database.DB.Debug().Create(&newUser).Error
+
+	if errorCreateUser != nil {
+		return ctx.Status(500).JSON(fiber.Map{
+			"message": "Error create user",
+		})
+	}
+
+	return ctx.JSON(fiber.Map{
+		"message": "Success create user",
+		"data":    newUser,
+	})
 }
--- route/index.route.go
+++ route/index.route.go
@@ -8,4 +8,5 @@ import (
 
 func RouteInit(r *fiber.App) {
 	r.Get("/user", controllers.UserControllerGetAll)
+	r.Post("/user", controllers.UserControllerCreate)
 }
