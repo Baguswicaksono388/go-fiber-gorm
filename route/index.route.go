44eb7102bea14b2ef5e8b142a1bfde0a92d01bdb
Bagus Wicaksono <baguswicaksono388@gmail.com>
2022-08-23 09:40:11 +0700

    static assets

--- route/index.route.go
+++ route/index.route.go
@@ -1,12 +1,14 @@
 package route
 
 import (
+	"go-fiber-gorm/config"
 	"go-fiber-gorm/controllers"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func RouteInit(r *fiber.App) {
+	r.Static("/public", config.ProjectRootPath+"/public/asset")
 	r.Get("/user", controllers.UserControllerGetAll)
 	r.Post("/user", controllers.UserControllerCreate)
 	r.Get("/user/:id", controllers.UserControllerGetById)
